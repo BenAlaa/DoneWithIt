03bb188fa32e66de96bd26f32719c27439df588c
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2020-06-25 16:45:05 +0200

    initialize app

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableHighlight, Image, SafeAreaView } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+
+export default function App() {
+  return (
+    // <WelcomeScreen/>
+    <ViewImageScreen/>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
